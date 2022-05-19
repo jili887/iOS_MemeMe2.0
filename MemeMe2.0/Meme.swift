31bfedff1a784eda183d9fb518072b0e4b9a36c3
jili887 <bettylijia@gmail.com>
2022-05-18 21:36:08 -0600

    Initial Commit-create project and meme struct model

---
+++ MemeMe2.0/Meme.swift
@@ -0,0 +1,21 @@
+//
+//  Meme.swift
+//  MemeMe2.0
+//
+//  Created by Jia Li on 5/18/22.
+//
+
+import Foundation
+import UIKit
+
+struct Meme{
+    
+    let topText: String!
+    
+    let bottomText: String!
+    
+    let image: UIImage?
+    
+    let memedImage: UIImage!
+    
+}
